8ea54fdb3e306116bf6d021e273365f588853e84
Davi Lopes Brito <162513480+davilb64@users.noreply.github.com>
2024-03-12 13:37:02 -0300

    Alguns Erros Corrigidos

--- encaminhamento.py
+++ encaminhamento.py
@@ -1,28 +1,28 @@
 import os
 import shutil
+import re
 
 pasta_origem = r'I:\Meu Drive\COMERCIAL\SAC\CONTRATOS DIGITALIZADOS NOVO\À ORGANIZAR'
-pasta_destino = r'C:\Users\Adm\Desktop\teste'  # Substitua pelo caminho da pasta de destino
+pasta_destino = r'I:\Meu Drive\COMERCIAL\SAC\CONTRATOS DIGITALIZADOS NOVO\temp'  # Substitua pelo caminho da pasta de destino
 
-# Função para extrair o número do nome do arquivo
+# Função para extrair o número do nome do arquivo usando expressões regulares
 def extrair_numero(nome_arquivo):
-    partes = nome_arquivo.split()
-    for parte in partes:
-        if parte.isdigit():
-            return parte
+    numero = re.search(r'\d+', nome_arquivo)
+    if numero:
+        return numero.group()
+    else:
+        return None
 
 # Percorre os arquivos na pasta de origem
 for _, _, arquivos in os.walk(pasta_origem):
     for arquivo in arquivos:
-        if any(x in arquivo for x in ["CONT", "DEV", "REM", "TROCA"]):
-            numero = extrair_numero(arquivo)
-            if numero:
-                pasta_numero = os.path.join(pasta_destino, numero)
-                try:
-                    if not os.path.exists(pasta_numero):
-                        os.makedirs(pasta_numero)
-                    shutil.move(os.path.join(pasta_origem, arquivo), os.path.join(pasta_numero, arquivo))
-                    print(f"Arquivo '{arquivo}' movido para '{pasta_numero}'")
-                except Exception as e:
-                    print(f"Erro ao mover o arquivo '{arquivo}': {str(e)}")
-
+        numero = extrair_numero(arquivo)
+        if numero:
+            pasta_numero = os.path.join(pasta_destino, numero)
+            try:
+                if not os.path.exists(pasta_numero):
+                    os.makedirs(pasta_numero)
+                shutil.move(os.path.join(pasta_origem, arquivo), pasta_numero)
+                print(f"Arquivo '{arquivo}' movido para '{pasta_numero}'")
+            except Exception as e:
+                print(f"Erro ao mover o arquivo '{arquivo}': {str(e)}")
